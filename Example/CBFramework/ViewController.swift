b16758b3be4ae6df75eaf7e57260296342326155
Handrata <handrata.febrianto@cashbac.com>
2021-09-08 10:15:11 +0700

    Update Example CBFramework

--- Example/CBFramework/ViewController.swift
+++ Example/CBFramework/ViewController.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
+//import CBFramework
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblBalance: UILabel!
+    
+    let pubKey = "pubKey001"
+    let partnerPubId = "partner01"
+    let userId = "user001"
+    
+    var cbToken: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+//        cbToken = CashbacFrameworkSDK.configure(withPubKey: pubKey,
+//                                                partnerPubId: partnerPubId,
+//                                                userIdentifier: userId)
+//        lblBalance.text = "\(CashbacFrameworkSDK.sharedInstance.getBalance().intValue)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
+extension ViewController {
+    @IBAction func btnPaymentMethodAction() {
+        //CashbacFrameworkSDK.sharedInstance.showPaymentMethodList(cbToken, parentViewController: self)
+    }
+    
+    @IBAction func btnPayWithCashbacAction() {
+        //CashbacFrameworkSDK.sharedInstance.showPaymentPage(cbToken, parentViewController: self)
+    }
+    
+    @IBAction func btnTransactionHistoryAction() {
+        //CashbacFrameworkSDK.sharedInstance.showPaymentHistory(cbToken, parentViewController: self)
+    }
+}
